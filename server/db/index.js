0b05d7d85556398d93a1b92a602ab5a35dae4ef7
Piyushrathoree <meonpath008@gmail.com>
2024-11-20 16:47:13 +0530

    some bug fixes

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,10 @@
-import mongoose from "mongoose";
+import { connect } from "mongoose";
 
 
 
-const connectDB = async () => {
+ const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
+        const connectionInstance = await connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
@@ -12,4 +12,5 @@ const connectDB = async () => {
     }
 }
 
-  export default connectDB
+
+export default connectDB;
