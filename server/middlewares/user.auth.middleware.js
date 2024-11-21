dde6ddf938f61cf346a2130982080199ea849f85
Piyushrathoree <meonpath008@gmail.com>
2024-11-22 02:25:36 +0530

    completed both middlewares for user and admin

---
+++ server/middlewares/user.auth.middleware.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+async function userMiddleware(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({ error: "No token provided" });
+    }
+
+    const token = authHeader;
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decoded;
+
+        next();
+    } catch (error) {
+        return res.status(403).json({ error: "Invalid token" });
+    }
+}
+
+export default userMiddleware;
