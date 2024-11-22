45389c83bd1cb33bceb37257549eca5b3e2d6929
Piyushrathoree <meonpath008@gmail.com>
2024-11-23 02:57:17 +0530

    all files  tested and updated

--- server/app.js
+++ server/app.js
@@ -7,11 +7,11 @@ app.use(express.json())
 //importing the routes
 import userRoute from "./routes/user.route.js"
 import adminRoute from "./routes/admin.route.js"
-
+import courseRoute from "./routes/course.route.js"
 
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/admin", adminRoute);
-
+app.use("/api/v1/course", courseRoute);
 
 export {app}
